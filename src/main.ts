a8063f033941148539fa20b1ddfcf9faa004ef74
Crownbackend <crownbackend@gmail.com>
2023-01-01 22:17:51 +0100

    add toast

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import router from './router'
 import store from './store'
 import "bootstrap";
 import 'bootstrap-icons/font/bootstrap-icons.css'
+import ToastPlugin from 'vue-toast-notification';
+//import 'vue-toast-notification/dist/theme-default.css';
+import 'vue-toast-notification/dist/theme-sugar.css';
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(ToastPlugin).mount('#app')
